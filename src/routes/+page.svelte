4d88f7e008ca02479042a7dc33cd1e75a82926cf
Jay Requizo <jay.requizo@outlook.com>
2025-12-10 22:56:01 +1100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<style>
+    .root {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+<div class="root">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
